3df3481d755e94797fc1879b9757a8acf3f81cea
Sérgio <84282812+sergioalves2002@users.noreply.github.com>
2021-11-23 18:42:42 +0000

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/Trabalho.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Trabalho.dir/comandos.cpp.obj"
+  "CMakeFiles/Trabalho.dir/main.cpp.obj"
+  "CMakeFiles/Trabalho.dir/map.cpp.obj"
+  "CMakeFiles/Trabalho.dir/trabalhadores.cpp.obj"
+  "Trabalho.exe"
+  "Trabalho.exe.manifest"
+  "Trabalho.lib"
+  "Trabalho.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Trabalho.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
